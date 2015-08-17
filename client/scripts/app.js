3bfe088fa962ddf8bf96544b755c468a4c9ec912
Kelly Johnson <kellyjohnson@kellyjohnson.local>
2015-08-17 17:44:36 -0500

    Code addes gender and modifies presentation of object data

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -7,21 +7,21 @@ console.log("Script Loaded");
 //var $SCOPE.name =... set to whatever the input field says.
 //Anytime you reference a variable, you reference $SCOPE.variable.
 //
-//    var myApp = angular.module('myApp', []);                            //pre-Angular js that we have to write
-//                                                                        //Standard convention is to have var name = angular.model('name').
-//                                                                        //aka, the [] - says go grab the dependencies that I require.
-//    myApp.controller('WelcomeController', ["$scope", function($scope){
-//        $scope.heading = "Message: ";
-//
-//        $scope.updateMessage = function(){                              //Making up a function that we are tying to the scope called "update message".
-//            $scope.message = "Hello, " + $scope.name + "!";
-//        };
-//
-//        $scope.goodByeMessage = function(){                              //Making up a function that we are tying to the scope called "update message".
-//            $scope.message = "Goodbye, " + $scope.name + ", See you soon!";
-//        };
-//                                                                        //Here's us declaring a variable
-//    }]);                                                                   // Here, we are defining the controller for our app
+    var myApp = angular.module('myApp', []);                            //pre-Angular js that we have to write
+                                                                        //Standard convention is to have var name = angular.model('name').
+                                                                        //aka, the [] - says go grab the dependencies that I require.
+    myApp.controller('WelcomeController', ["$scope", function($scope){
+        $scope.heading = "Message: ";
+
+        $scope.updateMessage = function(){                              //Making up a function that we are tying to the scope called "update message".
+            $scope.message = "Hello, " + $scope.name + "!";
+        };
+
+        $scope.goodByeMessage = function(){                              //Making up a function that we are tying to the scope called "update message".
+            $scope.message = "Goodbye, " + $scope.name + ", See you soon!";
+        };
+                                                                        //Here's us declaring a variable
+    }]);                                                                   // Here, we are defining the controller for our app
 //                                                               PASS in the $scope, you will not find this in alot of textbooks.
 
 
@@ -43,7 +43,7 @@ console.log("Script Loaded");
 //    }]);
 
     //Will Now Try to Make a Navigation Menu
-     var myApp = angular.module('myApp', []);
+    // var myApp = angular.module('myApp', []);
 
      myApp.controller('FriendsList', ["$scope", function($scope){
          $scope.heading = "Friends and Family";
